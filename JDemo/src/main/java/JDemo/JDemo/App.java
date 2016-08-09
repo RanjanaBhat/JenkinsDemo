9a758d42269ef2b0c9381ef2ee2c3e0f608ac156
RanjanaBhat <ranjana.bhat@anchanto.com>
2016-08-09 11:25:27 +0530

    Project Added

---
+++ JDemo/src/main/java/JDemo/JDemo/App.java
@@ -0,0 +1,13 @@
+package JDemo.JDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JDemo/src/test/java/JDemo/JDemo/JDemoClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package JDemo.JDemo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+/**
+ * @author anchanto
+ *
+ */
+public class JDemoClass {
+
+	@Test
+	public void testDemo(){
+		System.out.println("Hello..! Welcome To Webhook Configuration!");
+	}
+}
